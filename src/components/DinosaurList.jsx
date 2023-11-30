3625ca8fdeb18b4db59a5c06fdd0edd18f2638b1
John Savin <johnsavin@proton.me>
2023-11-30 00:12:30 -0500

    adding styling to pages

--- src/components/DinosaurList.jsx
+++ src/components/DinosaurList.jsx
@@ -1,23 +1,55 @@
-import { useState, useEffect } from "react"
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { Card } from 'antd';
+
+const { Meta } = Card;
 
 
-export default function DinosaurList({environment}) {
+export default function DinosaurList({ environment }) {
+  
   const [dinosaurs, setDinosaurs] = useState()
-
+  
   useEffect(() => {
-fetch(`http://localhost:8080/dinosaurs/${environment}`)
-.then((res) => res.json())
-.then((data) => setDinosaurs(data))
-  .catch(console.error)
+    fetch(`http://localhost:8080/dinosaurs/${environment}`)
+    .then((res) => res.json())
+    .then((data) => setDinosaurs(data))
+    .catch(console.error)
   }, [])
 
+  return (
+    <>
+      <h1>{environment.toUpperCase()} Dinosaur Exhibit</h1>
+      {/* {dinosaurs && dinosaurs.map((dinosaur) =>
+        <p>{dinosaur.name}</p>
+      )} */}
+  
+  <Card
+  style={{
+    width: 300,
+  }}
+  cover={
+    <img
+    alt="Tyrannosaurus Rex"
+    src="https://unsplash.com/photos/brown-dinosaur-illustration-hYKG311mff8"
+    />
+  }
+  actions={[
+    <SettingOutlined key="setting" />,
+    <EditOutlined key="edit" />,
+    <EllipsisOutlined key="ellipsis" />,
+  ]}
+  >
+    <Meta
+      title={dinosaurs && dinosaurs.map((dinosaur) =>
+        dinosaur.name)}
+        description="This is the description"
+        />
+  </Card>
+        </>
 
-
-return (
-  <>
-  <h1>{environment.toUpperCase()} Dinosaur Exhibit</h1>
-  {dinosaurs && dinosaurs.map(dinosaur => 
-    dinosaur.name)}
-  </>
 )
 }
+
+
+
