cd7a6ea9b757df9c34a0fd9900d4014c340b87ef
John Savin <johnsavin@proton.me>
2023-12-07 15:44:24 -0500

    css updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,31 +8,31 @@ export default function Home() {
   const nav = useNavigate()
   return (
     <>
-    <Divider></Divider>
+    <Divider style={{ borderColor: 'rgb(103, 13, 13)'}}></Divider>
     <p className='homeDivider'>
       Roar into Adventure!
        Join the Young Dino Hunters Trio on a Prehistoric Quest! Journey with Our Explorers as they Unleash the Thrill of the Dig and Dinosaur Discovery.
       </p>
-    <Divider></Divider>
+    <Divider style={{ borderColor: 'rgb(103, 13, 13)'}}></Divider>
 
     <body>
       <section className='home-pics'>
         <div onClick={() => { nav('/land') }}>
           <Image
             width={750}
-            src="/trex.jpg" alt="a pic of a land carnivore" />
+            src="/trex.jpg" alt="Land Dinosaurs" />
         </div>
 
         <div onClick={() => { nav('/sea') }}>
           <Image
             width={750}
-            src="/mosaurus.png" alt="a pic of a water carnivore" />
+            src="/mosaurus.png" alt="Water Dinosaurs" />
         </div>
 
         <div onClick={() => { nav('/air') }}>
           <Image
             width={750}
-            src="/pterodactyl.png" alt="a pic of an air carnivore" />
+            src="/pterodactyl.png" alt="Air Dinosaurs" />
         </div>
       </section>
     </body>
