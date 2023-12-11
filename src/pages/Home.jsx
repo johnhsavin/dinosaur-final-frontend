b551eae41255745a49a281d1728bd081939fef7d
John Savin <johnsavin@proton.me>
2023-12-11 14:14:56 -0500

    home pics updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,21 +18,21 @@ export default function Home() {
       <body>
         <section className='home-pics'>
           <div className='image-container' onClick={() => { nav('/land') }}>
-            <Image
-              width={width}
+            <img
+              // width={width}
               src="/trex.jpg" alt="Land Dinosaurs" />
             <div class="overlay">Land</div>
           </div>
 
           <div className='image-container' onClick={() => { nav('/sea') }}>
-            <Image
+            <img
               width={width}
               src="https://blog-app-jhs.s3.amazonaws.com/Mosasaur1.jpg" alt="Water Dinosaurs" />
             <div class="overlay">Sea</div>
           </div>
 
           <div className='image-container' onClick={() => { nav('/air') }}>
-            <Image
+            <img
               width={width}
               src="/pterodactyl.png" alt="Air Dinosaurs" />
             <div class="overlay">Air</div>
